c73806580a12c4c5effd756b45e3c881a58f6d9a
vinitghaturle <vinitghaturle656@gmail.com>
2025-08-14 20:22:25 +0000

    day one

---
+++ variables/ver.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "vinit@gmail.com"
+var accoundPassword = "12564"
+accoundCity = "ngp"
+let accoundState;
+
+// accountId = 2 Not allowed
+
+accountEmail = "dguy@gmail.com"
+accoundPassword = "562378"
+accoundCity = "hgt"
+
+console.log(accountId);
+
+
+/*
+dont vuse var its not useful
+accoundCity = "ngp" dont do this
+*/
+console.table([accountEmail,accoundPassword,accoundCity,accoundState])
